679e8e1e0e868e1c4712317b83318175346b2db8
Luiz Fernando <luizfernandosv85@gmail.com>
2023-06-24 16:41:38 -0300

    Estrutura inicial de pastas

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Portal Web - Dados sobre a SRAG - SINDROME RESPIRATÓRIA AGUDA
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
